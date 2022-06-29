2ddfa350620e2993ba4ac1641b7820aa3bbbccc2
jbloup <jbloup@hotmail.fr>
2022-06-30 00:19:33 +0200

    feat(users): add email field in resourceUser

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'email' => $this->email,
             'firstname' => $this->firstname,
             'lastname' => $this->lastname,
             'avatar' => $this->avatar,
